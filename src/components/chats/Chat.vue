c1dbdb18182830016e32b1db8d04ed19bdaffb42
Unknown <namv221296@gmail.com>
2021-01-24 12:01:58 +0700

    let's do this

---
+++ src/components/chats/Chat.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="flex px-4 py-2 hover:bg-dark rounded cp">
+      <img :src="user.avatar" class="w-12 h-12 rounded-full" alt />
+      <div class="px-4">
+        <h3>{{ user.name }}</h3>
+        <p class="text-sm text-placeholder">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["user"]
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home py-4">
+    <div v-for="friend in friends" :key="friend.id">
+      <Chat :user="friend" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Chat from "@/components/chats/Chat";
+
+export default {
+  name: "home",
+  props: ["friends"],
+  components: {
+    Chat
+  }
+};
+</script>
